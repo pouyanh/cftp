b91b1fe0017fb2d74be83ad5c316735c9a32b528
Pouyan Heyratpour <pouyan@ronaksoft.com>
2016-10-04 16:58:10 +0330

    List of todos
    Signed-off-by: Pouyan Heyratpour <pouyan@ronaksoft.com>

--- transfer_test.go
+++ transfer_test.go
@@ -1,7 +1,58 @@
 package cft
 
-import "testing"
+import (
+  "os"
+  "fmt"
+  "testing"
+  "io/ioutil"
+  "cmd/go/testdata/testinternal3"
+)
 
 func TestTransfer(t *testing.T) {
+  var filename string
+  if name, err := sampleRandom(t); err != nil {
+    t.Fatalf("Could create sample file", err)
+  } else {
+    filename = name
+  }
 
+  fmt.Println(filename)
+
+  // TODO: Instantiate client
+
+  // TODO: Instantiate server
+
+  // TODO: Open and Chunkize temp file. Calculate sha256
+
+  // TODO: Try to transfer file via client.
+
+  // TODO: Calculate sha256 of received file
+}
+
+func sampleRandom(t *testing.T) (string, error) {
+  var fr, fw *os.File
+
+  if fh, err := os.Open("/dev/urandom"); err != nil {
+    t.Fatalf("Could not open /dev/urandom", err)
+    return nil, err
+  } else {
+    fr = fh
+    defer fr.Close()
+  }
+
+  if fh, err := ioutil.TempFile(os.TempDir(), "cft"); err != nil {
+    t.Fatalf("Could not open temp file to write", err)
+    return nil, err
+  } else {
+    fw = fh
+    defer fw.Close()
+  }
+
+  b := make([]byte, 1e3)
+  for i := 0; i < 1e3; i++ {
+    fr.Read(b)
+    fw.Write(b)
+  }
+
+  return fw.Name(), nil
 }
